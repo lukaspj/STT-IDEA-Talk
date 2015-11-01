3e319faa589af3b472bd846bbc48b0c863a14e82
LukasPJ <ljorgensen@winterleafentertainment.com>
2015-11-01 22:44:52 +0100

    Added Ctrl+D to Exercise2

--- FirstSteps/src/sparetime/teaching/exercises/Exercise2.java
+++ FirstSteps/src/sparetime/teaching/exercises/Exercise2.java
@@ -2,7 +2,7 @@ package sparetime.teaching.exercises;
 
 ///////////////////////////////////////////////////
 // Simple code generation and refactoring.
-// In this exercise, we look at two keybindings:
+// In this exercise, we look at three keybindings:
 // Ctrl+Alt+T: The "Surround With" command, which
 //  lets you encapsulate a code block within two
 //  braces, an if-condition or something similar.
@@ -11,6 +11,7 @@ package sparetime.teaching.exercises;
 //  to move it into and out of scopes as well as
 //  not place it in between two lines of a multi-
 //  line if-statement.
+// Ctrl+D: This duplicates a line of code.
 //
 // Smart templates:
 //  IntelliJ has some built-in templates, where if
@@ -39,6 +40,9 @@ public class Exercise2 {
         // Use the Ctrl+Alt+T command to make sure that _testVal2 is only four if val1 is false.
         _testVal2 = 4;
         _testVal2 = 6;
+
+        // Use Ctrl+D to duplicate this line five times, making it print 3-1-3-2-4.
+        System.out.println("3");
 
         _testVal3 = 0;
         // Use smart templates to create a for loop that calls "_testVal3++" 50 times (so it ends up with the value 50)
